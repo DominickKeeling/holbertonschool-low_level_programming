a88b340a513d22c6fb8a51c338cbb695403fae53
Dominick <6157@holbertonstudents.com>
2023-07-17 21:12:17 -0700

    went through and compiled

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,18 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * get_bit - Returns the value of a bit at a given index
- * @n: Number to be evaluated
- * @index: What bit to return
- * Return: Value of bit at index
+ * set_bit - Sets the value of a bit to 1 at a given index
+ * @n: Integer that holds our bits
+ * @index: What bit to set to 1
+ * Return: 1 if worked, -1 if error occured
  */
 
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-	if ((n >> index) & 1)
-		return (1);
-	else
-		return (0);
+	*n |= (1 << index);
+	return (1);
 }
