8950214b02706d7bb6ab9a0602f636f298142099
dom <6157@holbertonstudents.com>
2023-12-22 21:05:56 -0600

    fixed output

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -9,26 +9,34 @@
  * Return: the index where the value is located. else -1
 */
 
-int binary_search(int *array, size_t size, int value) {
-   size_t left, right, mid;
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, mid;
+	size_t i;
 
-   if (!array) {
-       return -1;
-   }
+	if (array == NULL) {
+		return (-1);
+	}
 
-   left = 0;
-   right = size - 1;
+	left = 0;
+	right = size -1;
 
-   while (left <= right) {
-       mid = left + (right - left) / 2;
+	while (left <= right) {
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++) {
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+		
+		mid = left + (right - left) / 2;
 
-       if (array[mid] == value) {
-           return mid;
-       } else if (array[mid] < value) {
-           left = mid + 1;
-       } else {
-           right = mid - 1;
-       }
-   }
-   return -1;
+		if (array[mid] == value) {
+			return mid;
+		} else if (array[mid] < value) {
+			left = mid + 1;
+		} else {
+			right = mid -1;
+		}
+	}
+	return (-1);
 }
