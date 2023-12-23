7873fd7b65b42b433260771c90ee810936561d5a
dom <6157@holbertonstudents.com>
2023-12-22 21:30:42 -0600

    finally fixed delcarations

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -4,7 +4,7 @@
  * binary_search - Write a function that searches for a value in a sorted
  * array of integers using the Binary search algorithm
  * @array: pointer to the array
- * @size_t: the size of the array
+ * @size: the size of the array
  * @value: the value to search for
  * Return: the index where the value is located. else -1
 */
